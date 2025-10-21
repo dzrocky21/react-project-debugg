9ac06f559325e4a0a451820cde5e040078d85bb8
rocky <dzrocky21@gmail.com>
2025-10-21 09:00:41 +0100

    Reorganized project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './style/index.css'
 import './style/App.css'
@@ -11,9 +11,9 @@ import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <HashRouter>
     <App />
     </HashRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
